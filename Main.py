eb6afccd7d3b8f79b7322caad9f38779b5c535f0
Bubble0seven <stuart_hurdis@outlook.com>
2025-02-09 15:52:19 +0800

    Final Commit

---
+++ Main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Name Generator.")
+city = input("What City Did You Grow Up In?\n")
+pet = input("What is the name of your pet?\n")
+
+
+print("Your Band Name is " + city + " " + pet)
